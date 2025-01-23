57a99addda6099cedc14cbe16c0ca6969fd048ce
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-01-23 18:05:24 +0100

    Refactor ToArgs method and streamline exception handling
    Refactored the ToArgs method in various TestData record types to
    simplify ternary conditional expressions into single lines,
    improving readability and consistency. Also, streamlined the
    InvalidEnumArgumentException instantiation by directly throwing
    the exception within the throw statement.

--- TestDataTypes/TestData.cs
+++ TestDataTypes/TestData.cs
@@ -25,14 +25,11 @@ public abstract record TestData<TResult>(string Definition, string Result) : ITe
 
     public virtual object?[] ToArgs(ArgsCode argsCode)
     {
-        InvalidEnumArgumentException invalidEnumArgumentException = new(nameof(argsCode), (int)(object)argsCode, typeof(ArgsCode));
-        string message = "ITestData.ToArgs(ArgsCode.Properties) was not implemented in the inherited type.";
-
         return argsCode switch
         {
             ArgsCode.Instance => [this],
             ArgsCode.Properties => [TestCase],
-            _ => throw invalidEnumArgumentException,
+            _ => throw new InvalidEnumArgumentException(nameof(argsCode), (int)(object)argsCode, typeof(ArgsCode)),
         };
     }
 
@@ -43,61 +40,47 @@ public record TestData<String, T1>(string Definition, string Result, T1? Arg1)
     : TestData<string>(Definition, Result), ITestData<String>
 {
     public override object?[] ToArgs(ArgsCode argsCode)
-    => argsCode == ArgsCode.Properties ?
-        base.ToArgs(argsCode).Append(Arg1)
-        : base.ToArgs(argsCode);
+    => argsCode == ArgsCode.Properties ? base.ToArgs(argsCode).Append(Arg1) : base.ToArgs(argsCode);
 }
 
 public record TestData<String, T1, T2>(string Definition, string Result, T1? Arg1, T2? Arg2)
     : TestData<string, T1>(Definition, Result, Arg1)
 {
     public override object?[] ToArgs(ArgsCode argsCode)
-    => argsCode == ArgsCode.Properties ?
-        base.ToArgs(argsCode).Append(Arg2)
-        : base.ToArgs(argsCode);
+    => argsCode == ArgsCode.Properties ? base.ToArgs(argsCode).Append(Arg2) : base.ToArgs(argsCode);
 }
 
 public record TestData<String, T1, T2, T3>(string Definition, string Result, T1? Arg1, T2? Arg2, T3? Arg3)
     : TestData<string, T1, T2>(Definition, Result, Arg1, Arg2)
 {
     public override object?[] ToArgs(ArgsCode argsCode)
-    => argsCode == ArgsCode.Properties ?
-        base.ToArgs(argsCode).Append(Arg3)
-        : base.ToArgs(argsCode);
+    => argsCode == ArgsCode.Properties ? base.ToArgs(argsCode).Append(Arg3) : base.ToArgs(argsCode);
 }
 
 public record TestData<String, T1, T2, T3, T4>(string Definition, string Result, T1? Arg1, T2? Arg2, T3? Arg3, T4? Arg4)
     : TestData<string, T1, T2, T3>(Definition, Result, Arg1, Arg2, Arg3)
 {
     public override object?[] ToArgs(ArgsCode argsCode)
-    => argsCode == ArgsCode.Properties ?
-        base.ToArgs(argsCode).Append(Arg4)
-        : base.ToArgs(argsCode);
+    => argsCode == ArgsCode.Properties ? base.ToArgs(argsCode).Append(Arg4) : base.ToArgs(argsCode);
 }
 
 public record TestData<String, T1, T2, T3, T4, T5>(string Definition, string Result, T1? Arg1, T2? Arg2, T3? Arg3, T4? Arg4, T5? Arg5)
     : TestData<string, T1, T2, T3, T4>(Definition, Result, Arg1, Arg2, Arg3, Arg4)
 {
     public override object?[] ToArgs(ArgsCode argsCode)
-    => argsCode == ArgsCode.Properties ?
-        base.ToArgs(argsCode).Append(Arg5)
-        : base.ToArgs(argsCode);
+    => argsCode == ArgsCode.Properties ? base.ToArgs(argsCode).Append(Arg5) : base.ToArgs(argsCode);
 }
 
 public record TestData<String, T1, T2, T3, T4, T5, T6>(string Definition, string Result, T1? Arg1, T2? Arg2, T3? Arg3, T4? Arg4, T5? Arg5, T6? Arg6)
     : TestData<string, T1, T2, T3, T4, T5>(Definition, Result, Arg1, Arg2, Arg3, Arg4, Arg5)
 {
     public override object?[] ToArgs(ArgsCode argsCode)
-    => argsCode == ArgsCode.Properties ?
-        base.ToArgs(argsCode).Append(Arg6)
-        : base.ToArgs(argsCode);
+    => argsCode == ArgsCode.Properties ? base.ToArgs(argsCode).Append(Arg6) : base.ToArgs(argsCode);
 }
 
 public record TestData<String, T1, T2, T3, T4, T5, T6, T7>(string Definition, string Result, T1? Arg1, T2? Arg2, T3? Arg3, T4? Arg4, T5? Arg5, T6? Arg6, T7? Arg7)
     : TestData<string, T1, T2, T3, T4, T5, T6>(Definition, Result, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6)
 {
     public override object?[] ToArgs(ArgsCode argsCode)
-    => argsCode == ArgsCode.Properties ?
-        base.ToArgs(argsCode).Append(Arg7)
-        : base.ToArgs(argsCode);
+    => argsCode == ArgsCode.Properties ? base.ToArgs(argsCode).Append(Arg7) : base.ToArgs(argsCode);
 }
