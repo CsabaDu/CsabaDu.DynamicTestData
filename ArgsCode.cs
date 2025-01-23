02ef628d90864e6cfe8388a0daf2b6727c705754
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-01-23 18:33:10 +0100

    Add XML docs to ArgsCode enum in CsabaDu.DynamicTestData
    Added XML documentation comments to the ArgsCode enum, including summaries for the enum itself and its members, Instance and Properties. This improves code readability and maintainability by explaining the purpose of each enum value.

--- ArgsCode.cs
+++ ArgsCode.cs
@@ -1,7 +1,17 @@
 ﻿namespace CsabaDu.DynamicTestData;
 
+/// <summary>
+/// Specifies the different argument codes that can be used.
+/// </summary>
 public enum ArgsCode
 {
+    /// <summary>
+    /// Represents an instance argument code .
+    /// </summary>
     Instance,
+
+    /// <summary>
+    /// Represents a properties argument code.
+    /// </summary>
     Properties,
 }
