740ef88c3a36c740cfee6c962c462f0be80c59c7
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-01-23 21:52:08 +0100

    Rename 'item' to 'param' in Append method of Extensions class
    The method `Append` in the `Extensions` class has been updated.
    The type parameter `<T>` and the parameter `item` in the method
    signature and the method body have been renamed to `param`.
    Additionally, the XML documentation comments have been updated
    to reflect this change, replacing references to "item" with "param".

--- TestDataTypes/Extensions.cs
+++ TestDataTypes/Extensions.cs
@@ -5,15 +5,15 @@ public static class Extensions
     /// <summary>
     /// Appends an item to the end of the given array.
     /// </summary>
-    /// <typeparam name="T">The type of the item to append.</typeparam>
-    /// <param name="args">The array to which the item will be appended.</param>
-    /// <param name="item">The item to append to the array.</param>
-    /// <returns>A new array with the item appended to the end.</returns>
-    public static object?[] Append<T>(this object?[] args, T? item)
+    /// <typeparam name="T">The type of the parameter to append.</typeparam>
+    /// <param name="args">The array to which the parameter will be appended.</param>
+    /// <param name="param">The parameter to append to the array.</param>
+    /// <returns>A new array with the parameter appended to the end.</returns>
+    public static object?[] Append<T>(this object?[] args, T? param)
     {
         var result = new object?[args.Length + 1];
         Array.Copy(args, result, args.Length);
-        result[args.Length] = item;
+        result[args.Length] = param;
 
         return result;
     }
