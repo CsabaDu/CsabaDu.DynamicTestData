651b86f0f5f8f7d0f7b4582d6cc7ff8ef5984746
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-01-26 10:05:59 +0100

    Simplify method name in ExtensionsTests class
    Renamed method `ObjectArray_Add_arg_ArgsCode_arg_paramsObjectArray_returnsExpected`
    to `ObjectArray_Add_args_returnsExpected` for better readability.

--- _Tests/UnitTests/ExtensionsTests.cs
+++ _Tests/UnitTests/ExtensionsTests.cs
@@ -5,7 +5,7 @@ public sealed class ExtensionsTests
     private readonly object[] _sut = new ExtensionsTestsDataSources().Sut;
 
     [Theory, MemberData(nameof(ExtensionsTestsDataSources.AddArgsList), MemberType = typeof(ExtensionsTestsDataSources))]
-    public void ObjectArray_Add_arg_ArgsCode_arg_paramsObjectArray_returnsExpected(ArgsCode argsCode, string parameter, object[] expected)
+    public void ObjectArray_Add_args_returnsExpected(ArgsCode argsCode, string parameter, object[] expected)
     {
         // Arrange
         // Act
