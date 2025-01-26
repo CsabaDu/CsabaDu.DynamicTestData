0218fe2c0ac78ab07728d7e6dcf6eadec1b9a3a2
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-01-26 09:55:28 +0100

    Refactor test data sources for ExtensionsTests
    Removed ExtensionsDynamicDataSources class and its methods.
    Replaced with new ExtensionsTestsDataSources class.
    Updated ExtensionsTests to use the new data source.
    Introduced TheoryData property AddArgsList for test data.

---
+++ _Tests/DynamicDataSources/ExtensionsTestsDataSources.cs
@@ -0,0 +1,20 @@
+﻿namespace CsabaDu.DynamicTestData.Tests.DynamicDataSources;
+
+public class ExtensionsTestsDataSources()
+{
+    private static readonly object[] args = [null, 1];
+
+    public readonly object[] Args = args;
+
+    public static TheoryData<ArgsCode, string, object[]> AddArgsList => new()
+    {
+        // Returns object array with same elements.
+        { ArgsCode.Instance, "test", args },
+
+        // Returns the object array with the new null element.
+        { ArgsCode.Properties, "test", [.. args, "test"] },
+
+        // Returns the object array with the new null element.
+        { ArgsCode.Properties, null, [.. args, null] }
+    };
+}
--- _Tests/UnitTests/ExtensionsTests.cs
+++ _Tests/UnitTests/ExtensionsTests.cs
@@ -2,23 +2,18 @@
 
 public sealed class ExtensionsTests
 {
-    private static readonly ExtensionsDynamicDataSources DataSource = new();
+    private static readonly ExtensionsTestsDataSources DataSource = new();
     private readonly object[] _args = DataSource.Args;
 
-    public static IEnumerable<object[]> AddArgsList => DataSource.Add_ArgsToList();
-
-    [Theory, MemberData(nameof(AddArgsList))]
-    public void ObjectArray_Add_arg_ArgsCode_arg_paramsObjectArray_returnsExpected(TestDataRecord data)
+    [Theory, MemberData(nameof(ExtensionsTestsDataSources.AddArgsList), MemberType = typeof(ExtensionsTestsDataSources))]
+    public void ObjectArray_Add_arg_ArgsCode_arg_paramsObjectArray_returnsExpected(ArgsCode argsCode, string parameter, object[] expected)
     {
         // Arrange
-        var argsCode = (ArgsCode)data.Params[0];
-        var parameter = (string)data.Params[1];
-        var expected = (object[])data.Params[2];
-
         // Act
         var actual = _args.Add(argsCode, parameter);
 
         // Assert
         Assert.Equal(expected, actual);
     }
+
 }
