a9607e066133a72b5dbea949fe57e70d9662ca97
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-01-27 09:16:38 +0100

    Refactor: use constant for "test" in AddArgsList property
    Introduce a new private static readonly string field `Parameter`
    with the value "test". Replace the hardcoded string "test" in the
    `AddArgsList` property with the `Parameter` field to improve
    maintainability and consistency.

--- _Tests/DynamicDataSources/ExtensionsTestsDataSources.cs
+++ _Tests/DynamicDataSources/ExtensionsTestsDataSources.cs
@@ -3,14 +3,15 @@
 public class ExtensionsTestsDataSources()
 {
     public static readonly object[] Args = [null, 1];
+    private static readonly string Parameter = "test";
 
     public static TheoryData<ArgsCode, string, object[]> AddArgsList => new()
     {
         // Returns the object array with same elements.
-        { ArgsCode.Instance, "test", Args },
+        { ArgsCode.Instance, Parameter, Args },
 
         // Returns the same object array with the new null element.
-        { ArgsCode.Properties, "test", [.. Args, "test"] },
+        { ArgsCode.Properties, Parameter, [.. Args, Parameter] },
 
         // Returns the sam me object array with the new null element.
         { ArgsCode.Properties, null, [.. Args, null] }
