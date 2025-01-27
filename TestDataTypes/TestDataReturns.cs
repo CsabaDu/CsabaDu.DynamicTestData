bb9934416fcc46db23637c6cf3eb06240e281376
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-01-27 12:58:18 +0100

    Refactor test data classes and interfaces
    Updated `DynamicDataSource` methods to remove redundant `string` type parameter from `TestData` instantiation. Modified `ITestData` interface: renamed `Result` to `ResultName`, added `ExitMode`, and replaced `ITestData<String>` with `ITestData<TResult>`. Simplified `ITestDataReturns` by removing `Expected` property. Modified `ITestDataThrows` to extend `ITestData<Type>` and commented out `ExceptionType`.
    
    Refactored `TestData` class and derived records: removed generic type parameter, updated `TestCase` to use `ResultName` and `ExitMode`, made `ResultName` and `ExitMode` public and virtual. Updated `TestDataReturns` records to replace `Expected` with `Returns`, changed `ExitMode` to public, and added `ResultName`. Updated `TestDataThrows` to remove generic type parameter, moved and changed `ExitMode` to public, replaced `ExceptionType` with `Result` and `ResultName`, and updated `ToArgs` method.
    
    Simplified `TestDataChild` by removing `TResult` and `ExitMode`, and updated constructor to only take `Definition`.
    
    These changes improve clarity, consistency, and simplify class structures.

--- TestDataTypes/TestDataReturns.cs
+++ TestDataTypes/TestDataReturns.cs
@@ -6,22 +6,28 @@
 /// </summary>
 /// <typeparam name="TStruct">The type of the expected return value, which must be a struct.</typeparam>
 /// <param name="Definition">The definition of the test data.</param>
-/// <param name="Expected">The expected return value of the test data.</param>
-public abstract record TestDataReturns<TStruct>(string Definition, TStruct Expected)
-    : TestData<TStruct>(Definition, Expected.ToString()!), ITestDataReturns<TStruct>
+/// <param name="Returns">The expected return value of the test data.</param>
+public abstract record TestDataReturns<TStruct>(string Definition, TStruct Result)
+    : TestData(Definition), ITestDataReturns<TStruct>
     where TStruct : struct
 {
     // <summary>
     // Gets the expected exit mode of the test, which is "returns" for this type.
     // </summary>
-    protected override sealed string ExitMode => "returns";
+    public override sealed string ExitMode => "returns";
 
     /// <summary>
     /// Converts the test data to an array of arguments based on the specified argument code.
     /// </summary>
     /// <param name="argsCode">The code indicating which arguments to include.</param>
     /// <returns>An array of arguments.</returns>
-    public override object?[] ToArgs(ArgsCode argsCode) => base.ToArgs(argsCode).Add(argsCode, Expected);
+    public override object?[] ToArgs(ArgsCode argsCode) => base.ToArgs(argsCode).Add(argsCode, Result);
+
+    /// <summary>
+    /// Gets the result name of the test case.
+    /// </summary>
+    public override sealed string ResultName => Result.ToString()!;
+
 }
 #endregion
 
@@ -32,10 +38,10 @@ public abstract record TestDataReturns<TStruct>(string Definition, TStruct Expec
 /// <typeparam name="TStruct">The type of the expected return value, which must be a struct.</typeparam>
 /// <typeparam name="T1">The type of the first argument.</typeparam>
 /// <param name="Definition">The definition of the test data.</param>
-/// <param name="Expected">The expected return value of the test data.</param>
+/// <param name="Returns">The expected return value of the test data.</param>
 /// <param name="Arg1">The first argument.</param>
-public record TestDataReturns<TStruct, T1>(string Definition, TStruct Expected, T1? Arg1)
-    : TestDataReturns<TStruct>(Definition, Expected)
+public record TestDataReturns<TStruct, T1>(string Definition, TStruct Returns, T1? Arg1)
+    : TestDataReturns<TStruct>(Definition, Returns)
     where TStruct : struct
 {
     /// <summary>
@@ -53,11 +59,11 @@ public record TestDataReturns<TStruct, T1>(string Definition, TStruct Expected, 
 /// <typeparam name="T1">The type of the first argument.</typeparam>
 /// <typeparam name="T2">The type of the second argument.</typeparam>
 /// <param name="Definition">The definition of the test data.</param>
-/// <param name="Expected">The expected return value of the test data.</param>
+/// <param name="Returns">The expected return value of the test data.</param>
 /// <param name="Arg1">The first argument.</param>
 /// <param name="Arg2">The second argument.</param>
-public record TestDataReturns<TStruct, T1, T2>(string Definition, TStruct Expected, T1? Arg1, T2? Arg2)
-    : TestDataReturns<TStruct, T1>(Definition, Expected, Arg1)
+public record TestDataReturns<TStruct, T1, T2>(string Definition, TStruct Returns, T1? Arg1, T2? Arg2)
+    : TestDataReturns<TStruct, T1>(Definition, Returns, Arg1)
     where TStruct : struct
 {
     /// <summary>
@@ -76,12 +82,12 @@ public record TestDataReturns<TStruct, T1, T2>(string Definition, TStruct Expect
 /// <typeparam name="T2">The type of the second argument.</typeparam>
 /// <typeparam name="T3">The type of the third argument.</typeparam>
 /// <param name="Definition">The definition of the test data.</param>
-/// <param name="Expected">The expected return value of the test data.</param>
+/// <param name="Returns">The expected return value of the test data.</param>
 /// <param name="Arg1">The first argument.</param>
 /// <param name="Arg2">The second argument.</param>
 /// <param name="Arg3">The third argument.</param>
-public record TestDataReturns<TStruct, T1, T2, T3>(string Definition, TStruct Expected, T1? Arg1, T2? Arg2, T3? Arg3)
-    : TestDataReturns<TStruct, T1, T2>(Definition, Expected, Arg1, Arg2)
+public record TestDataReturns<TStruct, T1, T2, T3>(string Definition, TStruct Returns, T1? Arg1, T2? Arg2, T3? Arg3)
+    : TestDataReturns<TStruct, T1, T2>(Definition, Returns, Arg1, Arg2)
     where TStruct : struct
 {
     /// <summary>
@@ -101,13 +107,13 @@ public record TestDataReturns<TStruct, T1, T2, T3>(string Definition, TStruct Ex
 /// <typeparam name="T3">The type of the third argument.</typeparam>
 /// <typeparam name="T4">The type of the fourth argument.</typeparam>
 /// <param name="Definition">The definition of the test data.</param>
-/// <param name="Expected">The expected return value of the test data.</param>
+/// <param name="Returns">The expected return value of the test data.</param>
 /// <param name="Arg1">The first argument.</param>
 /// <param name="Arg2">The second argument.</param>
 /// <param name="Arg3">The third argument.</param>
 /// <param name="Arg4">The fourth argument.</param>
-public record TestDataReturns<TStruct, T1, T2, T3, T4>(string Definition, TStruct Expected, T1? Arg1, T2? Arg2, T3? Arg3, T4? Arg4)
-    : TestDataReturns<TStruct, T1, T2, T3>(Definition, Expected, Arg1, Arg2, Arg3)
+public record TestDataReturns<TStruct, T1, T2, T3, T4>(string Definition, TStruct Returns, T1? Arg1, T2? Arg2, T3? Arg3, T4? Arg4)
+    : TestDataReturns<TStruct, T1, T2, T3>(Definition, Returns, Arg1, Arg2, Arg3)
     where TStruct : struct
 {
     /// <summary>
@@ -128,14 +134,14 @@ public record TestDataReturns<TStruct, T1, T2, T3, T4>(string Definition, TStruc
 /// <typeparam name="T4">The type of the fourth argument.</typeparam>
 /// <typeparam name="T5">The type of the fifth argument.</typeparam>
 /// <param name="Definition">The definition of the test data.</param>
-/// <param name="Expected">The expected return value of the test data.</param>
+/// <param name="Returns">The expected return value of the test data.</param>
 /// <param name="Arg1">The first argument.</param>
 /// <param name="Arg2">The second argument.</param>
 /// <param name="Arg3">The third argument.</param>
 /// <param name="Arg4">The fourth argument.</param>
 /// <param name="Arg5">The fifth argument.</param>
-public record TestDataReturns<TStruct, T1, T2, T3, T4, T5>(string Definition, TStruct Expected, T1? Arg1, T2? Arg2, T3? Arg3, T4? Arg4, T5? Arg5)
-    : TestDataReturns<TStruct, T1, T2, T3, T4>(Definition, Expected, Arg1, Arg2, Arg3, Arg4)
+public record TestDataReturns<TStruct, T1, T2, T3, T4, T5>(string Definition, TStruct Returns, T1? Arg1, T2? Arg2, T3? Arg3, T4? Arg4, T5? Arg5)
+    : TestDataReturns<TStruct, T1, T2, T3, T4>(Definition, Returns, Arg1, Arg2, Arg3, Arg4)
     where TStruct : struct
 {
     /// <summary>
@@ -157,15 +163,15 @@ public record TestDataReturns<TStruct, T1, T2, T3, T4, T5>(string Definition, TS
 /// <typeparam name="T5">The type of the fifth argument.</typeparam>
 /// <typeparam name="T6">The type of the sixth argument.</typeparam>
 /// <param name="Definition">The definition of the test data.</param>
-/// <param name="Expected">The expected return value of the test data.</param>
+/// <param name="Returns">The expected return value of the test data.</param>
 /// <param name="Arg1">The first argument.</param>
 /// <param name="Arg2">The second argument.</param>
 /// <param name="Arg3">The third argument.</param>
 /// <param name="Arg4">The fourth argument.</param>
 /// <param name="Arg5">The fifth argument.</param>
 /// <param name="Arg6">The sixth argument.</param>
-public record TestDataReturns<TStruct, T1, T2, T3, T4, T5, T6>(string Definition, TStruct Expected, T1? Arg1, T2? Arg2, T3? Arg3, T4? Arg4, T5? Arg5, T6? Arg6)
-    : TestDataReturns<TStruct, T1, T2, T3, T4, T5>(Definition, Expected, Arg1, Arg2, Arg3, Arg4, Arg5)
+public record TestDataReturns<TStruct, T1, T2, T3, T4, T5, T6>(string Definition, TStruct Returns, T1? Arg1, T2? Arg2, T3? Arg3, T4? Arg4, T5? Arg5, T6? Arg6)
+    : TestDataReturns<TStruct, T1, T2, T3, T4, T5>(Definition, Returns, Arg1, Arg2, Arg3, Arg4, Arg5)
     where TStruct : struct
 {
     /// <summary>
@@ -188,7 +194,7 @@ public record TestDataReturns<TStruct, T1, T2, T3, T4, T5, T6>(string Definition
 /// <typeparam name="T6">The type of the sixth argument.</typeparam>
 /// <typeparam name="T7">The type of the seventh argument.</typeparam>
 /// <param name="Definition">The definition of the test data.</param>
-/// <param name="Expected">The expected return value of the test data.</param>
+/// <param name="Returns">The expected return value of the test data.</param>
 /// <param name="Arg1">The first argument.</param>
 /// <param name="Arg2">The second argument.</param>
 /// <param name="Arg3">The third argument.</param>
@@ -196,8 +202,8 @@ public record TestDataReturns<TStruct, T1, T2, T3, T4, T5, T6>(string Definition
 /// <param name="Arg5">The fifth argument.</param>
 /// <param name="Arg6">The sixth argument.</param>
 /// <param name="Arg7">The seventh argument.</param>
-public record TestDataReturns<TStruct, T1, T2, T3, T4, T5, T6, T7>(string Definition, TStruct Expected, T1? Arg1, T2? Arg2, T3? Arg3, T4? Arg4, T5? Arg5, T6? Arg6, T7? Arg7)
-    : TestDataReturns<TStruct, T1, T2, T3, T4, T5, T6>(Definition, Expected, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6)
+public record TestDataReturns<TStruct, T1, T2, T3, T4, T5, T6, T7>(string Definition, TStruct Returns, T1? Arg1, T2? Arg2, T3? Arg3, T4? Arg4, T5? Arg5, T6? Arg6, T7? Arg7)
+    : TestDataReturns<TStruct, T1, T2, T3, T4, T5, T6>(Definition, Returns, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6)
     where TStruct : struct
 {
     /// <summary>
@@ -221,7 +227,7 @@ public record TestDataReturns<TStruct, T1, T2, T3, T4, T5, T6, T7>(string Defini
 /// <typeparam name="T7">The type of the seventh argument.</typeparam>
 /// <typeparam name="T8">The type of the eighth argument.</typeparam>
 /// <param name="Definition">The definition of the test data.</param>
-/// <param name="Expected">The expected return value of the test data.</param>
+/// <param name="Returns">The expected return value of the test data.</param>
 /// <param name="Arg1">The first argument.</param>
 /// <param name="Arg2">The second argument.</param>
 /// <param name="Arg3">The third argument.</param>
@@ -230,8 +236,8 @@ public record TestDataReturns<TStruct, T1, T2, T3, T4, T5, T6, T7>(string Defini
 /// <param name="Arg6">The sixth argument.</param>
 /// <param name="Arg7">The seventh argument.</param>
 /// <param name="Arg8">The eighth argument.</param>
-public record TestDataReturns<TStruct, T1, T2, T3, T4, T5, T6, T7, T8>(string Definition, TStruct Expected, T1? Arg1, T2? Arg2, T3? Arg3, T4? Arg4, T5? Arg5, T6? Arg6, T7? Arg7, T8? Arg8)
-    : TestDataReturns<TStruct, T1, T2, T3, T4, T5, T6, T7>(Definition, Expected, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7)
+public record TestDataReturns<TStruct, T1, T2, T3, T4, T5, T6, T7, T8>(string Definition, TStruct Returns, T1? Arg1, T2? Arg2, T3? Arg3, T4? Arg4, T5? Arg5, T6? Arg6, T7? Arg7, T8? Arg8)
+    : TestDataReturns<TStruct, T1, T2, T3, T4, T5, T6, T7>(Definition, Returns, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7)
     where TStruct : struct
 {
     /// <summary>
@@ -256,7 +262,7 @@ public record TestDataReturns<TStruct, T1, T2, T3, T4, T5, T6, T7, T8>(string De
 /// <typeparam name="T8">The type of the eighth argument.</typeparam>
 /// <typeparam name="T9">The type of the ninth argument.</typeparam>
 /// <param name="Definition">The definition of the test data.</param>
-/// <param name="Expected">The expected return value of the test data.</param>
+/// <param name="Returns">The expected return value of the test data.</param>
 /// <param name="Arg1">The first argument.</param>
 /// <param name="Arg2">The second argument.</param>
 /// <param name="Arg3">The third argument.</param>
@@ -266,8 +272,8 @@ public record TestDataReturns<TStruct, T1, T2, T3, T4, T5, T6, T7, T8>(string De
 /// <param name="Arg7">The seventh argument.</param>
 /// <param name="Arg8">The eighth argument.</param>
 /// <param name="Arg9">The ninth argument.</param>
-public record TestDataReturns<TStruct, T1, T2, T3, T4, T5, T6, T7, T8, T9>(string Definition, TStruct Expected, T1? Arg1, T2? Arg2, T3? Arg3, T4? Arg4, T5? Arg5, T6? Arg6, T7? Arg7, T8? Arg8, T9? Arg9)
-    : TestDataReturns<TStruct, T1, T2, T3, T4, T5, T6, T7, T8>(Definition, Expected, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8)
+public record TestDataReturns<TStruct, T1, T2, T3, T4, T5, T6, T7, T8, T9>(string Definition, TStruct Returns, T1? Arg1, T2? Arg2, T3? Arg3, T4? Arg4, T5? Arg5, T6? Arg6, T7? Arg7, T8? Arg8, T9? Arg9)
+    : TestDataReturns<TStruct, T1, T2, T3, T4, T5, T6, T7, T8>(Definition, Returns, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8)
     where TStruct : struct
 {
     /// <summary>
