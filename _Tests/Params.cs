d383fbe05205801c8d5724744a491e00cff51a1c
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-01-28 16:19:33 +0100

    Update constants and adjust class closing brace
    Changed Arg2 to constant null, Arg5 to NegativeInfinity, and Arg8 to MaxValue. Moved closing brace of Params class to a new line.

--- _Tests/Params.cs
+++ _Tests/Params.cs
@@ -11,15 +11,14 @@ internal sealed class Params
     public static readonly string NotNullProperty = "Test Property";
 
     public const int Arg1 = 1;
-    public static readonly object Arg2 = new();
+    public const object Arg2 = null;
     public static readonly DateTime Arg3 = DateTime.MinValue;
     public const string Arg4 = "Test";
-    public const double Arg5 = double.MinValue;
+    public const double Arg5 = double.NegativeInfinity;
     public const bool Arg6 = true;
     public const char Arg7 = 'a';
-    public const float Arg8 = float.Tau;
+    public const float Arg8 = float.MaxValue;
     public static readonly object[] Arg9 = [];
 
     public static readonly TestDataChild TestData = new(ActualDefinition, ActualResult, ActualExitMode);
-
 }
