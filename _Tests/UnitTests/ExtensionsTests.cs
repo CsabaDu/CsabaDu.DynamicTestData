938ea78b40e98b9d14c11e252625015c51aeacdb
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-02-01 11:44:34 +0100

    Add new test method for Add in ExtensionsTests
    Added a new region labeled "Extensions tests" in the ExtensionsTests class.
    Introduced a new test method ObjectArray_Add_args_returnsExpected using [Theory, MemberData] to verify the Add method's behavior with various inputs.

--- _Tests/UnitTests/ExtensionsTests.cs
+++ _Tests/UnitTests/ExtensionsTests.cs
@@ -6,6 +6,7 @@ public sealed class ExtensionsTests
 {
     private readonly object[] _sut = ExtensionsArgs0;
 
+    #region Extensions tests
     [Theory, MemberData(nameof(AddTheoryData), MemberType = typeof(ExtensionsTheoryData))]
     public void ObjectArray_Add_args_returnsExpected(ArgsCode argsCode, string parameter, object[] expected)
     {
@@ -16,4 +17,5 @@ public sealed class ExtensionsTests
         // Assert
         Assert.Equal(expected, actual);
     }
+    #endregion
 }
