77eaa87dad103fddd825faa17e169be83eb15a58
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-02-05 07:59:22 +0100

    DynamicDataSource.TestDataToArgs methods string result param renamed to string expected.

--- DynamicDataSource.cs
+++ DynamicDataSource.cs
@@ -29,23 +29,23 @@ public abstract class DynamicDataSource(ArgsCode argsCode)
     /// </summary>
     /// <typeparam name="T1">The type of the first argument.</typeparam>
     /// <param name="definition">The definition of the test data.</param>
-    /// <param name="result">The expected result of the test.</param>
+    /// <param name="expected">The expected result of the test.</param>
     /// <param name="arg1">The first argument.</param>
     /// <returns>An array of arguments.</returns>
-    internal object?[] TestDataToArgs<T1>(string definition, string result, T1? arg1)
-    => new TestData<T1>(definition, result, arg1).ToArgs(ArgsCode);
+    internal object?[] TestDataToArgs<T1>(string definition, string expected, T1? arg1)
+    => new TestData<T1>(definition, expected, arg1).ToArgs(ArgsCode);
     /// <summary>
     /// Converts test data to an array of arguments.
     /// </summary>
     /// <typeparam name="T1">The type of the first argument.</typeparam>
     /// <typeparam name="T2">The type of the second argument.</typeparam>
     /// <param name="definition">The definition of the test data.</param>
-    /// <param name="result">The expected result of the test.</param>
+    /// <param name="expected">The expected result of the test.</param>
     /// <param name="arg1">The first argument.</param>
     /// <param name="arg2">The second argument.</param>
     /// <returns>An array of arguments.</returns>
-    internal object?[] TestDataToArgs<T1, T2>(string definition, string result, T1? arg1, T2? arg2)
-    => new TestData<T1, T2>(definition, result, arg1, arg2).ToArgs(ArgsCode);
+    internal object?[] TestDataToArgs<T1, T2>(string definition, string expected, T1? arg1, T2? arg2)
+    => new TestData<T1, T2>(definition, expected, arg1, arg2).ToArgs(ArgsCode);
 
     /// <summary>
     /// Converts test data to an array of arguments.
@@ -54,13 +54,13 @@ public abstract class DynamicDataSource(ArgsCode argsCode)
     /// <typeparam name="T2">The type of the second argument.</typeparam>
     /// <typeparam name="T3">The type of the third argument.</typeparam>
     /// <param name="definition">The definition of the test data.</param>
-    /// <param name="result">The expected result of the test.</param>
+    /// <param name="expected">The expected result of the test.</param>
     /// <param name="arg1">The first argument.</param>
     /// <param name="arg2">The second argument.</param>
     /// <param name="arg3">The third argument.</param>
     /// <returns>An array of arguments.</returns>
-    internal object?[] TestDataToArgs<T1, T2, T3>(string definition, string result, T1? arg1, T2? arg2, T3? arg3)
-    => new TestData<T1, T2, T3>(definition, result, arg1, arg2, arg3).ToArgs(ArgsCode);
+    internal object?[] TestDataToArgs<T1, T2, T3>(string definition, string expected, T1? arg1, T2? arg2, T3? arg3)
+    => new TestData<T1, T2, T3>(definition, expected, arg1, arg2, arg3).ToArgs(ArgsCode);
 
     /// <summary>
     /// Converts test data to an array of arguments.
@@ -70,14 +70,14 @@ public abstract class DynamicDataSource(ArgsCode argsCode)
     /// <typeparam name="T3">The type of the third argument.</typeparam>
     /// <typeparam name="T4">The type of the fourth argument.</typeparam>
     /// <param name="definition">The definition of the test data.</param>
-    /// <param name="result">The expected result of the test.</param>
+    /// <param name="expected">The expected result of the test.</param>
     /// <param name="arg1">The first argument.</param>
     /// <param name="arg2">The second argument.</param>
     /// <param name="arg3">The third argument.</param>
     /// <param name="arg4">The fourth argument.</param>
     /// <returns>An array of arguments.</returns>
-    internal object?[] TestDataToArgs<T1, T2, T3, T4>(string definition, string result, T1? arg1, T2? arg2, T3? arg3, T4? arg4)
-    => new TestData<T1, T2, T3, T4>(definition, result, arg1, arg2, arg3, arg4).ToArgs(ArgsCode);
+    internal object?[] TestDataToArgs<T1, T2, T3, T4>(string definition, string expected, T1? arg1, T2? arg2, T3? arg3, T4? arg4)
+    => new TestData<T1, T2, T3, T4>(definition, expected, arg1, arg2, arg3, arg4).ToArgs(ArgsCode);
 
     /// <summary>
     /// Converts test data to an array of arguments.
@@ -88,15 +88,15 @@ public abstract class DynamicDataSource(ArgsCode argsCode)
     /// <typeparam name="T4">The type of the fourth argument.</typeparam>
     /// <typeparam name="T5">The type of the fifth argument.</typeparam>
     /// <param name="definition">The definition of the test data.</param>
-    /// <param name="result">The expected result of the test.</param>
+    /// <param name="expected">The expected result of the test.</param>
     /// <param name="arg1">The first argument.</param>
     /// <param name="arg2">The second argument.</param>
     /// <param name="arg3">The third argument.</param>
     /// <param name="arg4">The fourth argument.</param>
     /// <param name="arg5">The fifth argument.</param>
     /// <returns>An array of arguments.</returns>
-    internal object?[] TestDataToArgs<T1, T2, T3, T4, T5>(string definition, string result, T1? arg1, T2? arg2, T3? arg3, T4? arg4, T5? arg5)
-    => new TestData<T1, T2, T3, T4, T5>(definition, result, arg1, arg2, arg3, arg4, arg5).ToArgs(ArgsCode);
+    internal object?[] TestDataToArgs<T1, T2, T3, T4, T5>(string definition, string expected, T1? arg1, T2? arg2, T3? arg3, T4? arg4, T5? arg5)
+    => new TestData<T1, T2, T3, T4, T5>(definition, expected, arg1, arg2, arg3, arg4, arg5).ToArgs(ArgsCode);
 
     /// <summary>
     /// Converts test data to an array of arguments.
@@ -108,7 +108,7 @@ public abstract class DynamicDataSource(ArgsCode argsCode)
     /// <typeparam name="T5">The type of the fifth argument.</typeparam>
     /// <typeparam name="T6">The type of the sixth argument.</typeparam>
     /// <param name="definition">The definition of the test data.</param>
-    /// <param name="result">The expected result of the test.</param>
+    /// <param name="expected">The expected result of the test.</param>
     /// <param name="arg1">The first argument.</param>
     /// <param name="arg2">The second argument.</param>
     /// <param name="arg3">The third argument.</param>
@@ -116,8 +116,8 @@ public abstract class DynamicDataSource(ArgsCode argsCode)
     /// <param name="arg5">The fifth argument.</param>
     /// <param name="arg6">The sixth argument.</param>
     /// <returns>An array of arguments.</returns>
-    internal object?[] TestDataToArgs<T1, T2, T3, T4, T5, T6>(string definition, string result, T1? arg1, T2? arg2, T3? arg3, T4? arg4, T5? arg5, T6? arg6)
-    => new TestData<T1, T2, T3, T4, T5, T6>(definition, result, arg1, arg2, arg3, arg4, arg5, arg6).ToArgs(ArgsCode);
+    internal object?[] TestDataToArgs<T1, T2, T3, T4, T5, T6>(string definition, string expected, T1? arg1, T2? arg2, T3? arg3, T4? arg4, T5? arg5, T6? arg6)
+    => new TestData<T1, T2, T3, T4, T5, T6>(definition, expected, arg1, arg2, arg3, arg4, arg5, arg6).ToArgs(ArgsCode);
 
     /// <summary>
     /// Converts test data to an array of arguments.
@@ -130,7 +130,7 @@ public abstract class DynamicDataSource(ArgsCode argsCode)
     /// <typeparam name="T6">The type of the sixth argument.</typeparam>
     /// <typeparam name="T7">The type of the seventh argument.</typeparam>
     /// <param name="definition">The definition of the test data.</param>
-    /// <param name="result">The expected result of the test.</param>
+    /// <param name="expected">The expected result of the test.</param>
     /// <param name="arg1">The first argument.</param>
     /// <param name="arg2">The second argument.</param>
     /// <param name="arg3">The third argument.</param>
@@ -139,8 +139,8 @@ public abstract class DynamicDataSource(ArgsCode argsCode)
     /// <param name="arg6">The sixth argument.</param>
     /// <param name="arg7">The seventh argument.</param>
     /// <returns>An array of arguments.</returns>
-    internal object?[] TestDataToArgs<T1, T2, T3, T4, T5, T6, T7>(string definition, string result, T1? arg1, T2? arg2, T3? arg3, T4? arg4, T5? arg5, T6? arg6, T7? arg7)
-    => new TestData<T1, T2, T3, T4, T5, T6, T7>(definition, result, arg1, arg2, arg3, arg4, arg5, arg6, arg7).ToArgs(ArgsCode);
+    internal object?[] TestDataToArgs<T1, T2, T3, T4, T5, T6, T7>(string definition, string expected, T1? arg1, T2? arg2, T3? arg3, T4? arg4, T5? arg5, T6? arg6, T7? arg7)
+    => new TestData<T1, T2, T3, T4, T5, T6, T7>(definition, expected, arg1, arg2, arg3, arg4, arg5, arg6, arg7).ToArgs(ArgsCode);
 
     /// <summary>
     /// Converts test data to an array of arguments.
@@ -154,7 +154,7 @@ public abstract class DynamicDataSource(ArgsCode argsCode)
     /// <typeparam name="T7">The type of the seventh argument.</typeparam>
     /// <typeparam name="T8">The type of the eighth argument.</typeparam>
     /// <param name="definition">The definition of the test data.</param>
-    /// <param name="result">The expected result of the test.</param>
+    /// <param name="expected">The expected result of the test.</param>
     /// <param name="arg1">The first argument.</param>
     /// <param name="arg2">The second argument.</param>
     /// <param name="arg3">The third argument.</param>
@@ -164,8 +164,8 @@ public abstract class DynamicDataSource(ArgsCode argsCode)
     /// <param name="arg7">The seventh argument.</param>
     /// <param name="arg8">The eighth argument.</param>
     /// <returns>An array of arguments.</returns>
-    internal object?[] TestDataToArgs<T1, T2, T3, T4, T5, T6, T7, T8>(string definition, string result, T1? arg1, T2? arg2, T3? arg3, T4? arg4, T5? arg5, T6? arg6, T7? arg7, T8? arg8)
-    => new TestData<T1, T2, T3, T4, T5, T6, T7, T8>(definition, result, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8).ToArgs(ArgsCode);
+    internal object?[] TestDataToArgs<T1, T2, T3, T4, T5, T6, T7, T8>(string definition, string expected, T1? arg1, T2? arg2, T3? arg3, T4? arg4, T5? arg5, T6? arg6, T7? arg7, T8? arg8)
+    => new TestData<T1, T2, T3, T4, T5, T6, T7, T8>(definition, expected, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8).ToArgs(ArgsCode);
 
     /// <summary>
     /// Converts test data to an array of arguments.
@@ -180,7 +180,7 @@ public abstract class DynamicDataSource(ArgsCode argsCode)
     /// <typeparam name="T8">The type of the eighth argument.</typeparam>
     /// <typeparam name="T9">The type of the ninth argument.</typeparam>
     /// <param name="definition">The definition of the test data.</param>
-    /// <param name="result">The expected result of the test.</param>
+    /// <param name="expected">The expected result of the test.</param>
     /// <param name="arg1">The first argument.</param>
     /// <param name="arg2">The second argument.</param>
     /// <param name="arg3">The third argument.</param>
@@ -191,8 +191,8 @@ public abstract class DynamicDataSource(ArgsCode argsCode)
     /// <param name="arg8">The eighth argument.</param>
     /// <param name="arg9">The ninth argument.</param>
     /// <returns>An array of arguments.</returns>
-    internal object?[] TestDataToArgs<T1, T2, T3, T4, T5, T6, T7, T8, T9>(string definition, string result, T1? arg1, T2? arg2, T3? arg3, T4? arg4, T5? arg5, T6? arg6, T7? arg7, T8? arg8, T9? arg9)
-    => new TestData<T1, T2, T3, T4, T5, T6, T7, T8, T9>(definition, result, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9).ToArgs(ArgsCode);
+    internal object?[] TestDataToArgs<T1, T2, T3, T4, T5, T6, T7, T8, T9>(string definition, string expected, T1? arg1, T2? arg2, T3? arg3, T4? arg4, T5? arg5, T6? arg6, T7? arg7, T8? arg8, T9? arg9)
+    => new TestData<T1, T2, T3, T4, T5, T6, T7, T8, T9>(definition, expected, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9).ToArgs(ArgsCode);
     #endregion
 
     #region TestDataReturnsToArgs
@@ -202,7 +202,7 @@ public abstract class DynamicDataSource(ArgsCode argsCode)
     /// <typeparam name="TStruct">The type of the expected result, which must be a struct.</typeparam>
     /// <typeparam name="T1">The type of the first argument.</typeparam>
     /// <param name="definition">The definition of the test data.</param>
-    /// <param name="expected">The expected result of the test.</param>
+    /// <param name="expected">The expected struct of the test.</param>
     /// <param name="arg1">The first argument.</param>
     /// <returns>An array of arguments.</returns>
     internal object?[] TestDataReturnsToArgs<TStruct, T1>(string definition, TStruct expected, T1? arg1) where TStruct : struct
@@ -215,7 +215,7 @@ public abstract class DynamicDataSource(ArgsCode argsCode)
     /// <typeparam name="T1">The type of the first argument.</typeparam>
     /// <typeparam name="T2">The type of the second argument.</typeparam>
     /// <param name="definition">The definition of the test data.</param>
-    /// <param name="expected">The expected result of the test.</param>
+    /// <param name="expected">The expected struct of the test.</param>
     /// <param name="arg1">The first argument.</param>
     /// <param name="arg2">The second argument.</param>
     /// <returns>An array of arguments.</returns>
@@ -230,7 +230,7 @@ public abstract class DynamicDataSource(ArgsCode argsCode)
     /// <typeparam name="T2">The type of the second argument.</typeparam>
     /// <typeparam name="T3">The type of the third argument.</typeparam>
     /// <param name="definition">The definition of the test data.</param>
-    /// <param name="expected">The expected result of the test.</param>
+    /// <param name="expected">The expected struct of the test.</param>
     /// <param name="arg1">The first argument.</param>
     /// <param name="arg2">The second argument.</param>
     /// <param name="arg3">The third argument.</param>
@@ -247,7 +247,7 @@ public abstract class DynamicDataSource(ArgsCode argsCode)
     /// <typeparam name="T3">The type of the third argument.</typeparam>
     /// <typeparam name="T4">The type of the fourth argument.</typeparam>
     /// <param name="definition">The definition of the test data.</param>
-    /// <param name="expected">The expected result of the test.</param>
+    /// <param name="expected">The expected struct of the test.</param>
     /// <param name="arg1">The first argument.</param>
     /// <param name="arg2">The second argument.</param>
     /// <param name="arg3">The third argument.</param>
@@ -266,7 +266,7 @@ public abstract class DynamicDataSource(ArgsCode argsCode)
     /// <typeparam name="T4">The type of the fourth argument.</typeparam>
     /// <typeparam name="T5">The type of the fifth argument.</typeparam>
     /// <param name="definition">The definition of the test data.</param>
-    /// <param name="expected">The expected result of the test.</param>
+    /// <param name="expected">The expected struct of the test.</param>
     /// <param name="arg1">The first argument.</param>
     /// <param name="arg2">The second argument.</param>
     /// <param name="arg3">The third argument.</param>
@@ -287,7 +287,7 @@ public abstract class DynamicDataSource(ArgsCode argsCode)
     /// <typeparam name="T5">The type of the fifth argument.</typeparam>
     /// <typeparam name="T6">The type of the sixth argument.</typeparam>
     /// <param name="definition">The definition of the test data.</param>
-    /// <param name="expected">The expected result of the test.</param>
+    /// <param name="expected">The expected struct of the test.</param>
     /// <param name="arg1">The first argument.</param>
     /// <param name="arg2">The second argument.</param>
     /// <param name="arg3">The third argument.</param>
@@ -310,7 +310,7 @@ public abstract class DynamicDataSource(ArgsCode argsCode)
     /// <typeparam name="T6">The type of the sixth argument.</typeparam>
     /// <typeparam name="T7">The type of the seventh argument.</typeparam>
     /// <param name="definition">The definition of the test data.</param>
-    /// <param name="expected">The expected result of the test.</param>
+    /// <param name="expected">The expected struct of the test.</param>
     /// <param name="arg1">The first argument.</param>
     /// <param name="arg2">The second argument.</param>
     /// <param name="arg3">The third argument.</param>
@@ -335,7 +335,7 @@ public abstract class DynamicDataSource(ArgsCode argsCode)
     /// <typeparam name="T7">The type of the seventh argument.</typeparam>
     /// <typeparam name="T8">The type of the eighth argument.</typeparam>
     /// <param name="definition">The definition of the test data.</param>
-    /// <param name="expected">The expected result of the test.</param>
+    /// <param name="expected">The expected struct of the test.</param>
     /// <param name="arg1">The first argument.</param>
     /// <param name="arg2">The second argument.</param>
     /// <param name="arg3">The third argument.</param>
@@ -362,7 +362,7 @@ public abstract class DynamicDataSource(ArgsCode argsCode)
     /// <typeparam name="T8">The type of the eighth argument.</typeparam>
     /// <typeparam name="T9">The type of the ninth argument.</typeparam>
     /// <param name="definition">The definition of the test data.</param>
-    /// <param name="expected">The expected result of the test.</param>
+    /// <param name="expected">The expected struct of the test.</param>
     /// <param name="arg1">The first argument.</param>
     /// <param name="arg2">The second argument.</param>
     /// <param name="arg3">The third argument.</param>
