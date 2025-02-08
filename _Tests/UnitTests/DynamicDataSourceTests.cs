63cf58eff45a569da1b985719e8e9d0dcc5a81bd
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-02-08 16:45:22 +0100

    GetDisplayName changed

--- _Tests/UnitTests/DynamicDataSourceTests.cs
+++ _Tests/UnitTests/DynamicDataSourceTests.cs
@@ -16,7 +16,7 @@ public sealed class DynamicDataSourceTests
         string testMethodName = nameof(DummyClass.DummyMethod);
         MethodInfo testMethod = typeof(DummyClass).GetMethod(testMethodName);
         object[] args = TestDataChildInstance.ToArgs(argsCode);
-        string expected = $"{testMethod.Name}({testData_colon_whitespace}{args[0] as string})";
+        string expected = $"{testMethod.Name}({args[0] as string})";
 
         // Act
         string actual = GetDisplayName(testMethod, args);
