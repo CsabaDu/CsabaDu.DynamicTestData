9246c56d9658518690ef3a5d026d67d71bec9a23
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-02-24 14:55:33 +0100

    merge globaUsings

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,5 +1,4 @@
 ﻿global using CsabaDu.DynamicTestData.DynamicDataSources;
+global using CsabaDu.DynamicTestData.NUnit.Statics;
 global using CsabaDu.DynamicTestData.Statics;
 global using CsabaDu.DynamicTestData.TestDataTypes;
-global using CsabaDu.DynamicTestData.TestDataTypes.Interfaces;
-global using System.ComponentModel;
