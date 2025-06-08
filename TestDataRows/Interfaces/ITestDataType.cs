f4fc89c55d6d8ad5850348ca05919725ca5b4a33
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-06-08 18:04:32 +0200

    clean

--- TestDataRows/Interfaces/ITestDataType.cs
+++
@@ -1,9 +0,0 @@
-﻿// SPDX-License-Identifier: MIT
-// Copyright (c) 2025. Csaba Dudas (CsabaDu)
-
-namespace CsabaDu.DynamicTestData.TestDataRows.Interfaces;
-
-public interface ITestDataType : IEqualityComparer<ITestDataType>
-{
-    Type TestDataType { get; }
-}
--- TestDataTypes/Interfaces/IArgsCode.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CsabaDu.DynamicTestData.TestDataTypes.Interfaces;
-
-public interface IArgsCode
-{
-    /// <summary>
-    /// Gets the <see cref="TestDataTypes.ArgsCode"/> type enum which determines the strategy of creating test parameters.
-    /// </summary>
-    ArgsCode ArgsCode { get; }
-}
