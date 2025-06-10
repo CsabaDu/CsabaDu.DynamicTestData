e812a338b1cd86ea63653aabe33091a3aeac8acf
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-06-10 10:36:13 +0200

    minor

--- _NUnit/TestDataTypes/TestCaseTestData.cs
+++ _NUnit/TestDataTypes/TestCaseTestData.cs
@@ -1,7 +1,7 @@
 ﻿// SPDX-License-Identifier: MIT
 // Copyright (c) 2025. Csaba Dudas (CsabaDu)
 
-namespace CsabaDu.DynamicTestData.NUnit.TestCaseTestDataTypes;
+namespace CsabaDu.DynamicTestData.NUnit.TestDataTypes;
 
 /// <summary>
 /// Represents a test case data class for NUnit.
