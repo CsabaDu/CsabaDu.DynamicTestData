0ae537b7a084fd40cd790fd94b815db7a89a5939
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-06-21 11:18:30 +0200

    minor cleanup

--- TestDataHolders/Interfaces/ICreateTestDataRow.cs
+++ TestDataHolders/Interfaces/ICreateTestDataRow.cs
@@ -7,6 +7,5 @@ public interface ICreateTestDataRow<TTestData, TRow>
 where TTestData : notnull, ITestData
 {
     ITestDataRow<TTestData, TRow> CreateTestDataRow(
-        TTestData testData/*,
-        IDataStrategy dataStrategy*/);
+        TTestData testData);
 }
