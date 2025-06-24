29317b705221db812b7342a4ac669c977d2cb250
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-06-24 15:45:42 +0200

    segregation

--- _TestHelpers/TestDoubles/DataStrategyObject.cs
+++
@@ -1,9 +0,0 @@
-﻿using CsabaDu.DynamicTestData.TestDataHolders.Interfaces;
-
-namespace CsabaDu.DynamicTestData.TestHelpers.TestDoubles;
-
-public sealed class DataStrategyObject(ArgsCode argsCode, bool? withExpected) : IDataStrategy
-{
-    public ArgsCode ArgsCode { get; } = argsCode;
-    public bool? WithExpected { get; } = withExpected;
-}
