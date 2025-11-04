18954d1813c9aae912536446bddd1f4515933686
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-11-04 12:09:34 +0100

    diagram, incr v2.1.1-beta

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,16 +1,16 @@
 ﻿// SPDX-License-Identifier: MIT
 // Copyright (c) 2025. Csaba Dudas (CsabaDu)
 
+global using CsabaDu.DynamicTestData.Core.Statics;
+global using CsabaDu.DynamicTestData.Core.TestDataTypes.Interfaces;
 global using CsabaDu.DynamicTestData.DataRowHolders;
 global using CsabaDu.DynamicTestData.DataRowHolders.Interfaces;
-global using CsabaDu.DynamicTestData.DataStrategyTypes.Interfaces;
-global using CsabaDu.DynamicTestData.DynamicDataSources;
-global using CsabaDu.DynamicTestData.Statics;
+global using CsabaDu.DynamicTestData.Lite.DataStrategyTypes.Interfaces;
+global using CsabaDu.DynamicTestData.Lite.DynamicDataSources;
 global using CsabaDu.DynamicTestData.TestDataRows;
 global using CsabaDu.DynamicTestData.TestDataRows.Interfaces;
-global using CsabaDu.DynamicTestData.TestDataTypes.Interfaces;
 global using System.Collections;
 global using System.Data;
 global using System.Diagnostics.CodeAnalysis;
+global using static CsabaDu.DynamicTestData.Core.TestDataTypes.TestDataFactory;
 global using static CsabaDu.DynamicTestData.DataStrategyTypes.DataStrategy;
-global using static CsabaDu.DynamicTestData.TestDataTypes.TestDataFactory;
