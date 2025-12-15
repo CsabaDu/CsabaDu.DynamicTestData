70405f5204f5c1acb225ef4794bcc601b3fdcaa8
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-12-15 19:08:46 +0100

    dg

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -2,7 +2,7 @@
 // Copyright (c) 2025. Csaba Dudas (CsabaDu)
 
 global using CsabaDu.DynamicTestData.Core.DataStrategyTypes;
-global using CsabaDu.DynamicTestData.Core.Extensions;
+global using CsabaDu.DynamicTestData.Core.Validators;
 global using CsabaDu.DynamicTestData.Core.TestDataTypes.Interfaces;
 global using CsabaDu.DynamicTestData.Lite.DataStrategyTypes.Interfaces;
 global using CsabaDu.DynamicTestData.Lite.DynamicDataSources;
