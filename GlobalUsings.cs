fe0e9ef94949aa89bac04ff44da4e097eb0e2526
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-12-21 21:17:14 +0100

    refactor

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -2,8 +2,9 @@
 // Copyright (c) 2025. Csaba Dudas (CsabaDu)
 
 global using CsabaDu.DynamicTestData.Core.DataStrategyTypes;
-global using CsabaDu.DynamicTestData.Core.Validators;
+global using CsabaDu.DynamicTestData.Core.TestDataTypes;
 global using CsabaDu.DynamicTestData.Core.TestDataTypes.Interfaces;
+global using CsabaDu.DynamicTestData.Core.Validators;
 global using CsabaDu.DynamicTestData.Lite.DataStrategyTypes.Interfaces;
 global using CsabaDu.DynamicTestData.Lite.DynamicDataSources;
 global using System.Collections;
